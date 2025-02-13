c18f48a0a98648af546d095e10cf04982d0c4091
Kamil <karatov.k.a@steos.io>
2025-02-13 15:53:12 +0300

    feat

--- app/components/navButton.tsx
+++ app/components/navButton.tsx
@@ -2,9 +2,7 @@ import Link from "next/link";
 export default function NavButton() {
   return (
     <div>
-      <Link href="/" className="block p-2 rounded">
-        SideBar
-      </Link>
+      <Link href="/" className="block p-2 rounded"></Link>
     </div>
   );
 }

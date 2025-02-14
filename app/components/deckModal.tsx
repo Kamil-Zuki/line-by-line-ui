287ce8105bf2260eeca2449f33cb995556d405bc
Kamil <karatov.k.a@steos.io>
2025-02-14 11:48:42 +0300

    feat: the deckModal

--- app/components/deckModal.tsx
+++ app/components/deckModal.tsx
@@ -23,42 +23,42 @@ const DeckModal: React.FC<ModalProps> = ({
   return (
     <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-lg w-96">
-        <h2 className="text-2xl font-bold mb-4">Create New Deck</h2>
+        <h2 className="text-black text-2xl font-bold mb-4">Create New Deck</h2>
 
         <form>
           <div className="mb-4">
-            <label className="block mb-2">Deck Title</label>
+            <label className="text-black block mb-2">Deck Title</label>
             <input
               type="text"
               name="title"
               value={newDeck.title}
               onChange={onChange}
-              className="w-full p-2 border rounded-md"
+              className="text-black w-full p-2 border rounded-md"
             />
           </div>
 
           <div className="mb-4">
-            <label className="block mb-2">Deck Description</label>
+            <label className="text-black block mb-2">Deck Description</label>
             <textarea
               name="description"
               value={newDeck.description}
               onChange={onChange}
-              className="w-full p-2 border rounded-md"
+              className="text-black w-full p-2 border rounded-md"
             />
           </div>
 
           <div className="mb-4">
-            <label className="block mb-2">Image URL</label>
+            <label className="text-black block mb-2">Image URL</label>
             <input
               type="text"
               name="imageUrl"
               value={newDeck.imageUrl}
               onChange={onChange}
-              className="w-full p-2 border rounded-md"
+              className="text-black w-full p-2 border rounded-md"
             />
           </div>
 
-          <div className="flex justify-between">
+          <div className="text-black flex justify-between">
             <button
               type="button"
               onClick={onClose}

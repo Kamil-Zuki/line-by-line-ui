54fd4427f72f9631234023e30ce11719dc81c124
Kamil <karatov.k.a@steos.io>
2025-02-20 09:34:04 +0300

    feat: add the libs folder

---
+++ app/middleware.ts
@@ -0,0 +1,8 @@
+// // middleware.js (at root)
+// import { NextResponse } from "next/server";
+
+// export function middleware(req: Request) {
+//   if (!req.cookies.auth) {
+//     return NextResponse.redirect("/login");
+//   }
+// }

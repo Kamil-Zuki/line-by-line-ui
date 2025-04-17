13c6db4ce06c557ee170824eaae10c80354a7785
Kamil <karatov.k.a@steos.io>
2025-04-17 08:34:32 +0300

    feat(api/review/id): add params to the POST method

--- app/api/personal-vocab/card/review/[id]/route.ts
+++ app/api/personal-vocab/card/review/[id]/route.ts
@@ -2,17 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 
 const API_URL = "http://85.175.218.17/api/v1/card";
 
-export async function POST(req: NextRequest) {
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   //#region Access token
   const accessToken = req?.cookies.get("accessToken")?.value;
   if (!accessToken)
     return NextResponse.json({ error: "Failed to log in" }, { status: 401 });
   //#endregion
 
-  const { cardId } = await req.json();
-
-  const response = await fetch(`${API_URL}/review/${cardId}`, {
-    method: "GET",
+  const response = await fetch(`${API_URL}/review/${params.id}`, {
+    method: "POST",
     headers: {
       Authorization: `Bearer ${accessToken}`,
       "Content-type": "application/json",

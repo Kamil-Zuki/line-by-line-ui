54c5716205983351d8312fc1abdfa06e6627f03e
Zuko <karatov7@gmail.com>
2025-04-19 16:25:37 +0300

    feat(deck-version/deck/[id]): add route.ts

---
+++ app/api/personal-vocab/deck-version/deck/id/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const API_URL = "http://85.175.218.17/api/v1/deck-version/deck";
+
+export async function GET(req: NextRequest, {props}: {props: {id: string}}) {
+  //#region Access token
+  const accessToken = req.cookies.get("accessToken")?.value;
+  if (!accessToken)
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+     //#endregion
+
+  try {
+
+    const response = await fetch(`${API_URL}/${props.id}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok)
+      throw NextResponse.json(
+        { error: "Failed to create deck subscriptions" },
+        { status: response.status }
+      );
+
+    const result = await response.json();
+
+    return NextResponse.json(result, {status: 200});
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}

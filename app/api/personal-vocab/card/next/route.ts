2ecec927653fbdc551b4f2f0eaf8c15e621c0cbc
Kamil <karatov.k.a@steos.io>
2025-05-13 15:53:41 +0300

    feat

---
+++ app/api/personal-vocab/card/next/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const API_URL = `${process.env.API_SERVER_ADDRESS}/api/v1/card/next`;
+
+export async function GET(req: NextRequest) {
+  //#region Access token
+  const accessToken = req?.cookies.get("accessToken")?.value;
+  if (!accessToken)
+    return NextResponse.json({ error: "Failed to log in" }, { status: 401 });
+  //#endregion
+
+  const response = await fetch(`${API_URL}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-type": "application/json",
+    },
+  });
+
+  if (!response.ok)
+    return NextResponse.json({ error: "Failed to get cards" }, { status: 500 });
+
+  const result = await response.json();
+
+  return NextResponse.json(result, { status: 200 });
+}
